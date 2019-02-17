edb44e3d847bc06e8ffdf7967268f99a375be7f2
tomkf <thomaskeithferris@gmail.com>
2019-02-17 01:10:03 +0000

    conditions

---
+++ kata1.js
@@ -0,0 +1,31 @@
+// For this kata, you'll be adding only the numbers in the array which match the given condition.
+
+// You'll be given an array of numbers and a condition, such as odd or even.
+// Given this condition, add up only the values which match that condition. If no values match the condition, return 0.
+
+let conditionalSum = function(values, condition) {
+  conditionSum = 0;
+  antiConditionSum = 0;
+  if (condition === "even") {
+    for (var i = 0; i < values.length; i++) {
+      if (values[i] % 2 === 0) {
+        conditionSum = conditionSum += values[i];
+      }
+    }
+    return conditionSum;
+  } else if (condition === "odd") {
+    for (var x = 0; x < values.length; x++) {
+      if (values[x] % 2 !== 0) {
+        antiConditionSum = antiConditionSum += values[x];
+      }
+    }
+    return antiConditionSum;
+  } else {
+    return 0;
+  }
+};
+
+console.log(conditionalSum([1, 2, 3, 4, 5], "even"));
+console.log(conditionalSum([1, 2, 3, 4, 5], "odd"));
+console.log(conditionalSum([13, 88, 12, 44, 99], "even"));
+console.log(conditionalSum([], "odd"));
