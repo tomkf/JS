fa4256c15d8fe029a48c0725273a1c8e35dab47f
Thomas Ferris <thomaskeithferris@gmail.com>
2019-03-01 21:37:03 -0500

    Add files via upload
    practice working with split function and conditionals....returns # of vowels in given argument

---
+++ countVowels.js
@@ -0,0 +1,26 @@
+let numberOfVowels = function(data) {
+  let vowelCounter = 0;
+  let createArray = data.split("");
+  for (i = 0; i < createArray.length; i++) {
+    if (createArray[i] === "a") {
+      vowelCounter++;
+    } else if (createArray[i] === "e") {
+      vowelCounter++;
+    } else if (createArray[i] === "i") {
+      vowelCounter++;
+    } else if (createArray[i] === "o") {
+      vowelCounter++;
+    } else if (createArray[i] === "u") {
+      vowelCounter++;
+    }
+  }
+  return vowelCounter;
+};
+
+console.log(numberOfVowels("orange"));
+console.log(numberOfVowels("lighthouse labs"));
+console.log(numberOfVowels("aeiou"));
+//expected output
+// 3
+// 5
+// 5
