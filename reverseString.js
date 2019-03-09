6118556435586c0e592a08e67a50a9b31608e01c
Thomas Ferris <thomaskeithferris@gmail.com>
2019-03-09 14:30:42 -0500

    3 ways to reverse a string

--- reverseString.js
+++ reverseString.js
@@ -1,3 +1,40 @@
+//with reverse method
+
+function reverseMehthod(str) {
+  let manipulateString = str.split("");
+  return manipulateString.reverse().join("");
+}
+
+console.log(reverseMehthod("upsideDown"));
+
+//output
+// nwoDedispu
+
+//with for loop
+
+function reverseString(string) {
+  let manipulateString = string.split("");
+  let newArray = [];
+  let strLength = string.length - 1;
+
+  for (let i = strLength; i >= 0; i--) {
+    let placeValue = manipulateString[i];
+    newArray.push(placeValue);
+  }
+
+  return newArray.join("");
+}
+
+console.log(reverseString("upsideDown"));
+
+//output
+// nwoDedispu
+
+
+
+//with a command line argument
+
+
 let CL = process.argv.slice(2);
 
 function reverseString(commandInput) {
