d373ca5a7c4bd91d35ede57710a38be53a9eed5d
Thomas Ferris <thomaskeithferris@gmail.com>
2019-03-10 18:34:26 -0400

    prints a given value X number of times

--- reapeatingNumbers.js
+++ reapeatingNumbers.js
@@ -2,15 +2,21 @@
 // The first will be the value to repeat, the second will be the amount of times to repeat that value.
 
 let repeatNumbers = function(data) {
+  let newarray = [];
   for (let i = 0; i < data.length; i++) {
     let subArray = data[i];
-    for (let x = 0; x < subArray.length; x++) {}
+    let repeatNumber = subArray[1];
+    let value = subArray[0];
+    for (let x = 0; x < repeatNumber; x++) {
+      newarray.push(value);
+    }
   }
+  return newarray.join(" ");
 };
 
 console.log(repeatNumbers([[1, 10]]));
-// console.log(repeatNumbers([[1, 2], [2, 3]]));
-// console.log(repeatNumbers([[10, 4], [34, 6], [92, 2]]));
+console.log(repeatNumbers([[1, 2], [2, 3]]));
+console.log(repeatNumbers([[10, 4], [34, 6], [92, 2]]));
 
 // Output
 // 1111111111
