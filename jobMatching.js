8cebcdbfc02599d0076555d33e9bd1f03e9d6826
Thomas Ferris <thomaskeithferris@gmail.com>
2019-03-11 16:46:26 -0400

    comparing object keys w/ conditionals

--- jobMatching.js
+++ jobMatching.js
@@ -1,24 +1,13 @@
-// // A candidate will have a minimum salary, so it will look like this:
-
-// // let candidate = {
-// //   minSalary: 120000
-// // }
-// // A job will have a maximum salary, so it will look like this:
-
-// // let job = {
-// //   maxSalary: 140000
-// // }
-// If either the candidate's minimum salary or the job's maximum salary is not present, throw an error.
-
 // // For a valid match, the candidate's minimum salary must be less than or equal to the job's maximum salary.
-// However, let's also include 10% wiggle room (deducted from the candidate's minimum salary)
+// Candidate's minimum salary included with 10% wiggle room (deducted from the candidate's minimum salary)
+// If either the candidate's minimum salary or the job's maximum salary is not present, throw an error.
 
 function match(candidate, job) {
   if (candidate.minSalary === undefined || job.maxSalary === undefined) {
     throw "slaray of an item not specified";
   }
-
-  if (candidate.minSalary <= job.maxSalary) {
+  let adjustAmount = candidate.minSalary / 1.1;
+  if (adjustAmount <= job.maxSalary) {
     return true;
   } else {
     return false;
