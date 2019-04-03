da60470d976be108cbb251d28fdb5fedeae8fbea
Thomas Ferris <thomaskeithferris@gmail.com>
2019-04-02 21:24:49 -0400

    generates chess board in command line, detects if queens threaten each other

--- queenDetector.js
+++ queenDetector.js
@@ -23,10 +23,32 @@ function generateBoard(whiteQueen, blackQueen) {
   console.log(board);
 }
 
-let whit3 = [0, 5];
+function queenThreat() {
+  let whiteQueen = white;
+  let blkQueen = black;
+  let white0 = whiteQueen[0];
+  let white1 = whiteQueen[1];
+  let diagonalValue1 = white1 - white0;
+  let black0 = blkQueen[0];
+  let black1 = blkQueen[1];
+  let diagonalValue2 = black1 - black0;
+
+  white0 === black0
+    ? console.log("Horizonal Threat: True")
+    : console.log("Horizonal Threat: False");
+  white1 === black1
+    ? console.log("Vertical Threat: True")
+    : console.log("Vertical Threat: False");
+
+  Math.abs(diagonalValue1) === Math.abs(diagonalValue2)
+    ? console.log("Diagonal Threat: True")
+    : console.log("Diagonal Threat: False");
+}
+
+let white = [0, 7];
 //
-let black = [5, 0];
+let black = [7, 0];
 
-generateBoard(whit3, black);
+generateBoard(white, black);
 
-function queenThreat() {}
+queenThreat();
